81798a889538287dcab0f61fa135b6ed1b033e8a
Noel da Costa <noeldacosta@gmail.com>
2024-06-19 12:25:21 +0100

    v1.0.3 - updated compatibility to v12

--- index.js
+++ index.js
@@ -1,41 +1,81 @@
-function n(n2) {
-  const e2 = Math.floor(n2), t2 = Math.round(1e10 * (n2 - e2)) / 1e10;
-  return `${e2}' ${Math.floor(12 * t2)}"`;
+function e(e2) {
+  const n2 = Math.floor(e2), a2 = Math.round(1e10 * (e2 - n2)) / 1e10;
+  return `${n2}' ${Math.floor(12 * a2)}"`;
 }
-function e(e2, t2 = "standing") {
-  const a2 = function(n2, e3 = "standing") {
-    return ("running" == e3 ? n2 : n2 / 2).toString();
-  }(e2, t2);
-  return n(a2);
+function n(e2, n2) {
+  let a2 = e2;
+  if (!n2.effects.size)
+    return e2;
+  let s2 = [];
+  for (let e3 of n2.effects)
+    if (console.log(e3), console.log(e3.changes), console.log(e3.changes.length), e3.changes?.length && !e3.isSuppressed && !e3.disabled)
+      for (let n3 of e3.changes)
+        console.log(n3), "dnd5e.foundryvtt-dnd5e-jump-calculator" === n3.key && s2.push(n3);
+  s2.sort((e3, n3) => (e3.priority || 0) - (n3.priority || 0));
+  for (let e3 of s2)
+    switch (console.log(e3), console.log(e3.mode), e3.mode) {
+      case 1:
+        a2 *= e3.value;
+        break;
+      case 2:
+        a2 += e3.value;
+        break;
+      case 3:
+        a2 = Math.min(a2, e3.value);
+        break;
+      case 4:
+        a2 = Math.max(a2, e3.value);
+        break;
+      case 5:
+        a2 = e3.value;
+    }
+  return a2;
 }
-function t(e2, t2 = "standing") {
-  const a2 = function(n2, e3 = "standing") {
-    return ("running" == e3 ? 3 + Number(n2) : (3 + Number(n2)) / 2).toString();
-  }(e2, t2);
-  return n(a2);
+function a(a2, s2, t2) {
+  const o2 = n(function(e2, n2) {
+    return ("running" == n2 ? e2 : e2 / 2).toString();
+  }(a2, s2), t2);
+  return e(o2);
 }
-function a(e2) {
-  const t2 = e2.split("."), a2 = Number(t2[0]), s2 = Number(t2[1]);
-  let l2 = a2 + a2 / 2, p = s2 + s2 / 2;
-  p > 12 && (l2 += 1, p -= 12);
-  const i = function(n2, e3) {
-    const t3 = 12 * Number(n2) + Number(e3);
-    return (t3 / 12).toFixed(1);
-  }(l2, p);
-  return console.log(i), n(i);
+function s(a2, s2, t2) {
+  const o2 = n(function(e2, n2) {
+    return ("running" == n2 ? 3 + Number(e2) : (3 + Number(e2)) / 2).toString();
+  }(a2, s2), t2);
+  return e(o2);
 }
-const s = '\n<div class="geoidesic-5e-jump-calc">\n    <h2>{{5eJumpCalc.longJumpTitle}}</h2>\n    <p>PHB p182</p>\n    <p><strong>Running jump</strong> ({{5eJumpCalc.10FtFirst}}): <span class="lozenge">{{running_jump}}</span></p>\n    <p><strong>Standing jump</strong>: <span class="lozenge">{{standing_jump}}</span></p>\n    {{5eJumpCalc.longJumpPost}}\n</div>\n', l = '\n<div class="geoidesic-5e-jump-calc">\n    <h2>High Jump</h2>\n    <p>PHB p182</p>\n    <p><strong>Running jump</strong> ({{5eJumpCalc.10FtFirst}}): <span class="lozenge">{{running_jump}}</span></p>\n    <p><strong>Standing jump</strong>: <span class="lozenge">{{standing_jump}}</span></p>\n    <p><strong>Reach</strong>: <span class="lozenge">+{{reach}}</span></p>\n    {{5eJumpCalc.highJumpPost}}\n</div>\n';
-Hooks.on("renderActorSheet5eCharacter", (n2, p, i) => {
-  p.find(".pills-group:last-of-type").after('\n    <div class="pills-group">\n        <h3 class="icon">\n            <i class="fas fa-person-running"></i>\n            <span class="roboto-upper">Jump Calculator</span>\n        </h3>\n        <ul class="pills">\n            <li class="pill geoidesic-5e-button">\n                <span class="label" id="renderHighJumpButton">High jump</span>\n            </li>\n            <li class="pill geoidesic-5e-button">\n                <span class="label" id="renderLongJumpButton">Long jump</span>\n            </li>\n        </ul>\n    </div>\n');
-  const c = n2.document.system.abilities.str.value || 10, u = n2.document.system.abilities.str.mod || 0, o = function(n3) {
-    const e2 = n3.match(/('|\s)(\d{1,3}")/gm);
-    return n3.match(/^(\d{1,3}|\d\s|\d")/gm)[0] + "." + e2[0].replace(/["\s]/g, "");
-  }(n2.document.system.details.height) || "6.0";
-  p.find("#renderHighJumpButton").on("click", () => {
-    ChatMessage.create({ content: l.replace("{{running_jump}}", t(u, "running")).replace("{{standing_jump}}", t(u, "standing")).replace("{{5eJumpCalc.highJumpTitle}}", game.i18n.localize("5eJumpCalc.highJumpTitle")).replace("{{5eJumpCalc.highJumpPost}}", game.i18n.localize("5eJumpCalc.highJumpPost")).replace("{{5eJumpCalc.10FtFirst}}", game.i18n.localize("5eJumpCalc.10FtFirst")).replace("{{reach}}", a(o)), speaker: ChatMessage.getSpeaker({ actor: n2.actor }) });
+function t(n2) {
+  const a2 = n2.split("."), s2 = Number(a2[0]), t2 = Number(a2[1]);
+  let o2 = s2 + s2 / 2, c2 = t2 + t2 / 2;
+  c2 > 12 && (o2 += 1, c2 -= 12);
+  const r2 = function(e2, n3) {
+    const a3 = 12 * Number(e2) + Number(n3);
+    return (a3 / 12).toFixed(1);
+  }(o2, c2);
+  return e(r2);
+}
+const o = '\n<div class="geoidesic-5e-jump-calc">\n    <h2>{{5eJumpCalc.longJumpTitle}}</h2>\n    <p>PHB p182</p>\n    <p><strong>Running jump</strong> ({{5eJumpCalc.10FtFirst}}): <span class="lozenge">{{running_jump}}</span></p>\n    <p><strong>Standing jump</strong>: <span class="lozenge">{{standing_jump}}</span></p>\n    {{5eJumpCalc.longJumpPost}}\n</div>\n', c = '\n<div class="geoidesic-5e-jump-calc">\n    <h2>High Jump</h2>\n    <p>PHB p182</p>\n    <p><strong>Running jump</strong> ({{5eJumpCalc.10FtFirst}}): <span class="lozenge">{{running_jump}}</span></p>\n    <p><strong>Standing jump</strong>: <span class="lozenge">{{standing_jump}}</span></p>\n    <p><strong>Reach</strong>: <span class="lozenge">+{{reach}}</span></p>\n    {{5eJumpCalc.highJumpPost}}\n</div>\n', r = '\n    <div class="pills-group">\n        <h3 class="icon">\n            <i class="fas fa-person-running"></i>\n            <span class="roboto-upper">Jump Calculator</span>\n        </h3>\n        <ul class="pills">\n            <li class="pill geoidesic-5e-button">\n                <span class="label" id="renderHighJumpButton">High jump</span>\n            </li>\n            <li class="pill geoidesic-5e-button">\n                <span class="label" id="renderLongJumpButton">Long jump</span>\n            </li>\n        </ul>\n    </div>\n', l = (e2, n2) => {
+  const r2 = e2.document.system.abilities.str.value || 10, l2 = e2.document.system.abilities.str.mod || 0, i = function(e3) {
+    if (!e3)
+      return null;
+    const n3 = e3.match(/('|\s)(\d{1,3}")/gm);
+    return e3.match(/^(\d{1,3}|\d\s|\d")/gm)[0] + "." + n3[0].replace(/["\s]/g, "");
+  }(e2.document.system.details.height);
+  n2.find("#renderHighJumpButton").on("click", () => {
+    console.log("app", e2), i ? ChatMessage.create({ content: c.replace("{{running_jump}}", s(l2, "running", e2.actor)).replace("{{standing_jump}}", s(l2, "standing", e2.actor)).replace("{{5eJumpCalc.highJumpTitle}}", game.i18n.localize("5eJumpCalc.highJumpTitle")).replace("{{5eJumpCalc.highJumpPost}}", game.i18n.localize("5eJumpCalc.highJumpPost")).replace("{{5eJumpCalc.10FtFirst}}", game.i18n.localize("5eJumpCalc.10FtFirst")).replace("{{reach}}", t(i)), speaker: ChatMessage.getSpeaker({ actor: e2.actor }) }) : ChatMessage.create({ content: "<h2>High Jump</h2><p>This character's height has not been set. Please set the height in the Biography tab of the character sheet.</p>", speaker: ChatMessage.getSpeaker({ actor: e2.actor }) });
   });
-  p.find("#renderLongJumpButton").on("click", () => {
-    ChatMessage.create({ content: s.replace("{{running_jump}}", e(c, "running")).replace("{{standing_jump}}", e(c, "standing")).replace("{{5eJumpCalc.longJumpTitle}}", game.i18n.localize("5eJumpCalc.longJumpTitle")).replace("{{5eJumpCalc.longJumpPost}}", game.i18n.localize("5eJumpCalc.longJumpPost")).replace("{{5eJumpCalc.10FtFirst}}", game.i18n.localize("5eJumpCalc.10FtFirst")), speaker: ChatMessage.getSpeaker({ actor: n2.actor }) });
+  n2.find("#renderLongJumpButton").on("click", () => {
+    ChatMessage.create({ content: o.replace("{{running_jump}}", a(r2, "running", e2.actor)).replace("{{standing_jump}}", a(r2, "standing", e2.actor)).replace("{{5eJumpCalc.longJumpTitle}}", game.i18n.localize("5eJumpCalc.longJumpTitle")).replace("{{5eJumpCalc.longJumpPost}}", game.i18n.localize("5eJumpCalc.longJumpPost")).replace("{{5eJumpCalc.10FtFirst}}", game.i18n.localize("5eJumpCalc.10FtFirst")), speaker: ChatMessage.getSpeaker({ actor: e2.actor }) });
   });
+};
+Hooks.once("ready", (e2, n2, a2) => {
+}), Hooks.on("renderActorSheet5eCharacter", (e2, n2, a2) => {
+  if (game.modules.has("tidy5e-sheet") && "Tidy5eCharacterSheet" === e2.constructor.name) {
+    const a3 = n2.find(".main-panel .small-gap"), s2 = $(r);
+    s2.addClass("tidy5e");
+    const t2 = s2.find(".pill");
+    t2.css("color", "white"), t2.css("border-radius", "3px"), t2.css("width", "5rem"), t2.css("margin", "2px 0"), t2.css("padding", "2px 5px"), t2.css("cursor", "pointer"), t2.css("background-color", "var(--t5e-checkbox-outline-color)"), t2.css("border", "1px solid var(--dnd5e-color-tan)"), a3.after(s2), l(e2, n2);
+  } else {
+    n2.find(".pills-group:last-of-type").after(r), l(e2, n2);
+  }
 });
 //# sourceMappingURL=index.js.map
